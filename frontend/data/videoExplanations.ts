bfcbba2d156c3fd0b31db8b53aa5dd644792d01e
WasabiPingKak <wasabi.pingkak@gmail.com>
2025-08-20 01:25:36 +0800

    Add 0805 the world solution

--- frontend/data/videoExplanations.ts
+++ frontend/data/videoExplanations.ts
@@ -5,6 +5,9 @@ const VIDEO_EXPLANATIONS: Record<string, Record<string, string>> = {
   "2025-08-04": {
     "the-world": "https://youtu.be/PJg_Ycn5cY4?si=vyUVqEpULWM644DA",
   },
+  "2025-08-05": {
+    "the-world": "https://youtu.be/LzKffwZb2TM?si=2E1tVOjufngWyh6D",
+  },
 };
 
 export default VIDEO_EXPLANATIONS;
